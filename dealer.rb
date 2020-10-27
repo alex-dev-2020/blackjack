205226b1c962ca2c10494465de262b602b8b3c15
alex-dev-2020 <ubuntu@ip-172-31-4-30.us-east-2.compute.internal>
2020-10-27 01:52:52 +0000

    Created Dealer class

---
+++ dealer.rb
@@ -0,0 +1,13 @@
+class Dealer < Gamer
+  def initialize(bank)
+    super(bank, 'Дилер')
+  end
+
+  def actions
+    if @sum >= 17 
+      return([:skip])
+    elsif @cards.size == 2
+      return([:add])
+    end    
+  end
+end
